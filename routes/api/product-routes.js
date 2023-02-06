d55783fcda340b93e95834bf8365f9ff6f7ab9e3
hannahsvaughan <hannahvaughan421@gmail.com>
2023-02-06 15:52:19 -0500

    working on getting routes to work - started on product

--- routes/api/product-routes.js
+++ routes/api/product-routes.js
@@ -9,7 +9,7 @@ router.get('/', async (req, res) => {
   // be sure to include it's associated Category and Tag data
   try {
     const productData = await Product.findAll({
-      include: [{ model: Category}, { model: Tag }],
+      include: [{ model: Category }, { model: Tag }],
     });
     res.status(200).json(productData);
   } catch (err) {
@@ -26,7 +26,7 @@ router.get('/:id', async (req, res) => {
       include: [{ model: Category }, { model: Tag }],
     });
     if (!productData) {
-      res.status(404).json({ message: 'No product found with that id!'})
+      res.status(404).json({ message: 'No product found with that id!' })
       return;
     }
     res.status(200).json(productData);

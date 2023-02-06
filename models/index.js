c58760c857e7665776c06f3278b0248e350a0927
hannahsvaughan <hannahvaughan421@gmail.com>
2023-02-06 15:10:39 -0500

    finished models

--- models/index.js
+++ models/index.js
@@ -5,12 +5,20 @@ const Tag = require('./Tag');
 const ProductTag = require('./ProductTag');
 
 // Products belongsTo Category
+Product.belongsTo(Category);
 
 // Categories have many Products
+Category.hasMany(Product);
 
 // Products belongToMany Tags (through ProductTag)
+Product.belongsTo(Tag, {
+  foreignKey: 'tag_id',
+});
 
 // Tags belongToMany Products (through ProductTag)
+Tag.belongsTo(Product, {
+  foreignKey: 'product_id',
+});
 
 module.exports = {
   Product,
